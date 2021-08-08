3cf895a9e89835e2500b22ab77b279b1c39708f2
Bartosz Celuch <bartosz.celuch@eclipsegroup.co.uk>
2021-08-08 21:27:40 +0200

    :test_tube: Add two more test

--- src/test/java/DateTest.java
+++ src/test/java/DateTest.java
@@ -22,41 +22,57 @@ class DateTest {
     }
 
     @Test
-    void addingMonthChangeMonthInDate() {
+    void addingMonthsChangeMonthInDate() {
         Date dateWithAddedMonth = date.changeMonths(date, 2);
         assertThat(dateWithAddedMonth.getMonth()).isEqualTo(9);
     }
 
     @Test
-    void addingDayChangeDayInDate() {
+    void addingDaysChangeDayInDate() {
         Date dateWithAddedDay = date.changeDays(date, 9);
         assertThat(dateWithAddedDay.getDay()).isEqualTo(31);
     }
 
     @Test
-    void addingAmountOfMonthsThatChangeYearByOne(SoftAssertions softly) {
+    void addingMonthsThatChangeYearByOne(SoftAssertions softly) {
         Date dateWithAddedMonths = date.changeMonths(date, 6);
         softly.assertThat(dateWithAddedMonths.getMonth()).isEqualTo(1);
         softly.assertThat(dateWithAddedMonths.getYear()).isEqualTo(1995);
     }
 
     @Test
-    void addingAmountOfDaysThatChangeMonthNumberInDateByOne(SoftAssertions softly) {
+    void addingDaysThatChangeMonthNumberInDateByOne(SoftAssertions softly) {
         Date dateWithAddedDays = date.changeDays(date, 15);
         softly.assertThat(dateWithAddedDays.getDay()).isEqualTo(6);
         softly.assertThat(dateWithAddedDays.getMonth()).isEqualTo(8);
     }
 
     @Test
-    void addingAmountOfDaysThatChangeMonthNumberInDateByTwo(SoftAssertions softly) {
+    void addingDaysThatChangeMonthNumberInDateByTwo(SoftAssertions softly) {
         Date dateWithAddedDays = date.changeDays(date, 42);
         softly.assertThat(dateWithAddedDays.getMonth()).isEqualTo(9);
         softly.assertThat(dateWithAddedDays.getDay()).isEqualTo(2);
     }
+
     @Test
-    void addingAmountOfDaysThatChangeMonthNumberInDateByThree(SoftAssertions softly) {
+    void addingDaysThatChangeMonthNumberInDateByThree(SoftAssertions softly) {
         Date dateWithAddedDays = date.changeDays(date, 70);
         softly.assertThat(dateWithAddedDays.getMonth()).isEqualTo(9);
         softly.assertThat(dateWithAddedDays.getDay()).isEqualTo(30);
+    }
+
+    @Test
+    void addingDaysThatChangeYearInDateByOne(SoftAssertions softly) {
+        Date dateWithAddedDays = date.changeDays(date, 164);
+        softly.assertThat(dateWithAddedDays.getDay()).isEqualTo(2);
+        softly.assertThat(dateWithAddedDays.getMonth()).isEqualTo(1);
+        softly.assertThat(dateWithAddedDays.getYear()).isEqualTo(1995);
+    }
+
+    @Test
+    void secondMonthInLeapYearHasTwentyNineDays() {
+        Date leapYear = new Date(1, 2, 2000);
+        int numberOfDaysInSecondMonth = leapYear.getNumberOfDaysInMonth(2, 2000);
+        assertThat(numberOfDaysInSecondMonth).isEqualTo(29);
     }
 }
