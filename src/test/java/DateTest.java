d60d1f2fe6b07dddf990ce2202323992ca0c32f6
Bartosz Celuch <bartosz.celuch@eclipsegroup.co.uk>
2021-08-08 18:20:26 +0200

    :test_tube: Add two tests

--- src/test/java/DateTest.java
+++ src/test/java/DateTest.java
@@ -1,8 +1,12 @@
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SoftAssertionsExtension.class)
 class DateTest {
     private Date date;
 
@@ -23,4 +27,23 @@ class DateTest {
         assertThat(dateWithAddedMonth.getMonth()).isEqualTo(9);
     }
 
+    @Test
+    void addingDayChangeDayInDate() {
+        Date dateWithAddedDay = date.changeDays(date, 9);
+        assertThat(dateWithAddedDay.getDay()).isEqualTo(31);
+    }
+
+    @Test
+    void addingMonthsWhenSumOfMonthsIsGreaterThanTwelveChangeMonthsAndYearsInDate(SoftAssertions softly) {
+        Date dateWithAddedMonths = date.changeMonths(date, 6);
+        softly.assertThat(dateWithAddedMonths.getMonth()).isEqualTo(1);
+        softly.assertThat(dateWithAddedMonths.getYear()).isEqualTo(1995);
+    }
+
+    @Test
+    void addingDayWhenSumOfDaysIsGreaterThanAmountOfDaysInThatMonthChangeDaysAndMonthsInDate(SoftAssertions softly) {
+        Date dateWithAddedDays = date.changeDays(date,15);
+        softly.assertThat(dateWithAddedDays.getDay()).isEqualTo(6);
+        softly.assertThat(dateWithAddedDays.getMonth()).isEqualTo(8);
+    }
 }
